900ea9c1d890334bf1bf8cee58e70549ee82724d
HishamSha3098 <ihthishamanu@gmail.com>
2023-04-25 19:11:37 +0530

    login,signup,saveshows

--- netflixreact/src/App.js
+++ netflixreact/src/App.js
@@ -1,15 +1,31 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import { AuthContextprovider } from "./context/AuthContext";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Account from "./pages/Account";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 
 function App() {
   return (
     <>
+    <AuthContextprovider>
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />}/>
+        <Route path='/login' element={<Login />}/>
+        <Route path='/signup' element={<Signup />}/>
+        <Route path='/account' element={
+        <ProtectedRoute>
+          <Account />
+          </ProtectedRoute>
+        }/>
+
+
       </Routes>
+      </AuthContextprovider>
     </>
   );
 }
